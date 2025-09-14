58ee51df9f874eea6147585cca313c0e094e0ae1
Mikhail Kulik <smeleoqw2020@gmail.com>
2025-09-15 01:56:58 +0300

    Second

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 class Data:
-    first_name = 'Мицл'
-    last_name = 'Кук'
-    user_name = 'mwhail'
-    email = 'miil.kulwwik@sprint.ru'
-    password = '123etgbt5vv54v5v'
+    first_name = 'Миццл'
+    last_name = 'Кчук'
+    user_name = 'mwheail'
+    email = 'miil.kulwweik@sprint.ru'
+    password = '123etgbt5v54v5v'
     recipe_name = 'Что-то вкусное'
     eel = 'угорь'
     weight = 2
