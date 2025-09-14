83885b6b029e3bcd6d66679dccf3e94c97cb28e0
Mikhail Kulik <smeleoqw2020@gmail.com>
2025-09-15 02:28:35 +0300

    f

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from url import Url
 
-
 @pytest.fixture(scope="function")
 def driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(options=chrome_options)
+
+    driver = webdriver.Remote(
+        command_executor="http://selenoid:4444/wd/hub",
+        options=chrome_options
+    )
+
     driver.get(Url.site)
     yield driver
     driver.quit()
